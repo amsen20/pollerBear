dfcf7a2d338cbc70f2c198685599ec85bee8407b
amsen <ahph1380@gmail.com>
2024-08-26 16:13:38 -0400

    Add try operations to epoll for having more control

--- pollerBear/src/main/scala/epoll/Epoll.scala
+++ pollerBear/src/main/scala/epoll/Epoll.scala
@@ -3,21 +3,24 @@ package epoll
 
 // TODO: test it!!
 
-import scala.scalanative.unsafe._
-import scala.scalanative.runtime._
-import scala.scalanative.unsigned._
-import scala.scalanative.posix.unistd._
-import scala.collection.mutable._
-
 import epollImplicits._
+import pollerBear.logger.PBLogger
+import scala.collection.mutable._
+import scala.scalanative.posix.errno
+import scala.scalanative.posix.unistd._
+import scala.scalanative.runtime._
+import scala.scalanative.unsafe._
+import scala.scalanative.unsigned._
 
 case class WaitEvent(fd: Int, events: EpollEvents)
 
-class Epoll(epollFd: Int, maxEvents: Int)(using Zone) {
+class Epoll(epollFd: Int, maxEvents: Int)(
+    using Zone
+) {
 
   // dummy pipe fds is for waking up epoll_wait
-  val dummyPipeFds = alloc[Int](2)
-  val dummyBuf = alloc[Byte](1)
+  val dummyPipeFds = alloc[Int](2) // [read, write]
+  val dummyBuf     = alloc[Byte](1)
 
   // the event object used for epoll_ctl
   val ev = alloc[epoll.epoll_event]()
@@ -26,39 +29,62 @@ class Epoll(epollFd: Int, maxEvents: Int)(using Zone) {
 
   def initDummyPipe(): Unit =
     if pipe(dummyPipeFds) != 0 then throw EpollCreateError()
+    Utils.setNonBlocking(dummyPipeFds(0)).foreach { errno =>
+      throw EpollInitiationError("setting pipe read fd to non-blocking", errno)
+    }
+    Utils.setNonBlocking(dummyPipeFds(1)).foreach { errno =>
+      throw EpollInitiationError("setting pipe write fd to non-blocking", errno)
+    }
     addFd(dummyPipeFds(0), EpollInputEvents().wakeUp().input())
 
   initDummyPipe()
 
-  private def epollCtl(fd: Int, what: Int, op: EpollEvents): Unit =
+  private def epollCtlSafe(fd: Int, what: Int, op: EpollEvents): Option[CInt] =
     val msk = op.getMask()
     ev.events = msk.toUInt
     ev.data = fd.toPtr[Byte]
 
-    // TODO check the fd flags
-    if epoll.epoll_ctl(epollFd, what, fd, ev) != 0 then
-      throw EpollCtlError(s"failed to ${what match
-          case epoll.EPOLL_CTL_ADD => "add"
-          case epoll.EPOLL_CTL_MOD => "modify"
-          case epoll.EPOLL_CTL_DEL => "delete"
-        } fd: ${fd} from epoll")
+    if epoll.epoll_ctl(epollFd, what, fd, ev) != 0 then Some(errno.errno)
+    else None
+
+  private def epollCtl(fd: Int, what: Int, op: EpollEvents): Unit =
+    epollCtlSafe(fd, what, op) match
+      case Some(errno) =>
+        throw EpollCtlError(
+          s"failed to ${what match
+              case epoll.EPOLL_CTL_ADD => "add"
+              case epoll.EPOLL_CTL_MOD => "modify"
+              case epoll.EPOLL_CTL_DEL => "delete"
+            } fd: ${fd} from epoll",
+          errno
+        )
+      case None => ()
 
   def addFd(fd: Int, op: EpollEvents): Unit =
     epollCtl(fd, epoll.EPOLL_CTL_ADD, op)
 
+  def tryAddFd(fd: Int, op: EpollEvents): Option[CInt] =
+    epollCtlSafe(fd, epoll.EPOLL_CTL_ADD, op)
+
   def modifyFd(fd: Int, op: EpollEvents): Unit =
     epollCtl(fd, epoll.EPOLL_CTL_MOD, op)
 
+  def tryModifyFd(fd: Int, op: EpollEvents): Option[CInt] =
+    epollCtlSafe(fd, epoll.EPOLL_CTL_MOD, op)
+
   def removeFd(fd: Int): Unit =
     epollCtl(fd, epoll.EPOLL_CTL_DEL, EpollEvents())
+
+  def tryRemoveFd(fd: Int): Option[CInt] =
+    epollCtlSafe(fd, epoll.EPOLL_CTL_DEL, EpollEvents())
 
   def waitForEvents(timeout: Int): (List[WaitEvent], Boolean) =
     val nfds = epoll.epoll_wait(epollFd, evs, maxEvents, timeout)
     if nfds < 0 then throw EpollWaitError()
     val waitEvents = ListBuffer.empty[WaitEvent]
     for i <- 0 until nfds do
-      val curEvent = evs + i
-      val curFd = curEvent.data.toInt
+      val curEvent  = evs + i
+      val curFd     = curEvent.data.toInt
       val curEvents = curEvent.events.toInt
       if curFd == dummyPipeFds(0) then
         // drain the dummy pipe
@@ -69,9 +95,11 @@ class Epoll(epollFd: Int, maxEvents: Int)(using Zone) {
 
   def wakeUp(): Unit =
     write(dummyPipeFds(1), dummyBuf, 1.toCSize)
+
 }
 
 object Epoll {
+
   def apply[T](maxEvents: Int = 64)(body: Epoll => T): T =
     Zone:
       val epollFd = epoll.epoll_create1(0)
@@ -79,4 +107,5 @@ object Epoll {
       try
         body(new Epoll(epollFd, maxEvents))
       finally close(epollFd)
+
 }
--- pollerBear/src/main/scala/epoll/EpollError.scala
+++ pollerBear/src/main/scala/epoll/EpollError.scala
@@ -1,12 +1,18 @@
 package pollerBear
 package epoll
 
-import scala.scalanative.libc.errno
+import scala.scalanative.posix.errno
+import scalanative.unsafe._
 
 sealed abstract class EpollError(msg: String) extends RuntimeException(msg)
 
-final case class EpollCtlError(what: String)
-    extends EpollError("an error occurred during epoll_ctl: " + what + " errno: " + errno.errno)
+final case class EpollInitiationError(what: String, errno: CInt)
+    extends EpollError(
+      "an error occurred during epoll initialization: " + what + " errno: " + errno
+    )
+
+final case class EpollCtlError(what: String, errno: CInt)
+    extends EpollError("an error occurred during epoll_ctl: " + what + " errno: " + errno)
 
 final case class EpollWaitError()
     extends EpollError("an error occurred during epoll_wait: errno: " + errno.errno)
